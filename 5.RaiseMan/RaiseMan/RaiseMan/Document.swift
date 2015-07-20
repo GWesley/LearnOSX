978518dcd34ce06735165033e9fe1ea151227da2
GWesley <zzycn2012@gmail.com>
2015-07-20 19:05:00 +0800

    add undo to RaiseMan

--- 5.RaiseMan/RaiseMan/RaiseMan/Document.swift
+++ 5.RaiseMan/RaiseMan/RaiseMan/Document.swift
@@ -8,9 +8,26 @@
 
 import Cocoa
 
-class Document: NSDocument {
+class Document: NSDocument, NSWindowDelegate {
     
-    var employees: [Employee] = []
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet var arrayController: NSArrayController!
+    
+    var employees: [Employee] = [] {
+        willSet {
+            for employee in employees {
+                stopObservingEmployee(employee)
+            }
+        }
+        
+        didSet {
+            for employee in employees {
+                startObservingEmployee(employee)
+            }
+        }
+    }
+    
+    private var KVOContext: Int = 0
     
     override init() {
         super.init()
@@ -43,6 +60,90 @@ class Document: NSDocument {
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
         throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+    }
+    
+    // MARK: - Actions
+    @IBAction func addEmployee(sender: NSButton) {
+        let windowController = windowControllers[0]
+        let window = windowController.window!
+        
+        let endedEditing = window.makeFirstResponder(window)
+        if !endedEditing {
+            print("Unable to end editing")
+            return
+        }
+        
+        let undo: NSUndoManager = undoManager!
+        if undo.groupingLevel > 0 {
+            undo.endUndoGrouping()
+            undo.beginUndoGrouping()
+        }
+        
+        //create object
+        let employee = arrayController.newObject()
+        arrayController.addObject(employee)
+        arrayController.rearrangeObjects()
+        
+        let sortedEmployees = arrayController.arrangedObjects
+        let row = sortedEmployees.indexOfObject(employee)
+        tableView.editColumn(0, row: row, withEvent: nil, select: true)
+    }
+    
+    // MARK: - Accessors
+    func insertObject(employee: Employee, inEmployeesAtIndex index: Int) {
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).removeObjectFromEmployeesAtIndex(employees.count)
+        
+        if !undo.undoing {
+            undo.setActionName("Add Person")
+        }
+        
+        employees.append(employee)
+    }
+    
+    
+    func removeObjectFromEmployeesAtIndex(index: Int) {
+        let employee: Employee = employees[index]
+        
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).insertObject(employee, inEmployeesAtIndex: index)
+        
+        if !undo.undoing {
+            undo.setActionName("Remove Person")
+        }
+        
+        employees.removeAtIndex(index)
+        
+    }
+    
+    // MARK: - Key Value Observing 
+    func startObservingEmployee(employee: Employee) {
+        employee.addObserver(self, forKeyPath: "name", options: .Old, context: &KVOContext)
+        employee.addObserver(self, forKeyPath: "raise", options: .Old, context: &KVOContext)
+    }
+    
+    func stopObservingEmployee(employee: Employee) {
+        employee.removeObserver(self, forKeyPath: "name", context: &KVOContext)
+        employee.removeObserver(self, forKeyPath: "raise", context: &KVOContext)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [NSObject : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if context != &KVOContext {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        if let oldValue = change?[NSKeyValueChangeOldKey] {
+            let undo: NSUndoManager = undoManager!
+            undo.prepareWithInvocationTarget(object!).setValue(oldValue, forKeyPath: keyPath!)
+        }
+        
+    }
+    
+    // MARK: - NSWindowDelegate
+    
+    func windowWillClose(notification: NSNotification) {
+        employees = []
     }
 
 
