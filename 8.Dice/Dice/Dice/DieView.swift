03f8424234b232fb8521b28485b833b78de957f5
GWesley <zzycn2012@gmail.com>
2015-08-29 20:28:18 +0800

    add keyboard event to Dice

--- 8.Dice/Dice/Dice/DieView.swift
+++ 8.Dice/Dice/Dice/DieView.swift
@@ -124,4 +124,47 @@ class DieView: NSView {
         }
         pressed = false
     }
+    
+    // MARK: - Keyboard Events
+    override func keyDown(theEvent: NSEvent) {
+        interpretKeyEvents([theEvent])
+    }
+    
+    override func insertText(insertString: AnyObject) {
+        let text = insertString as! String
+        if let number = Int(text) {
+            intValue = number
+        }
+    }
+    
+    override func insertTab(sender: AnyObject?) {
+        window?.selectNextKeyView(sender)
+    }
+    
+    override func insertBacktab(sender: AnyObject?) {
+        window?.selectPreviousKeyView(sender)
+    }
+    
+    // MARK: - First Responder
+    override var acceptsFirstResponder: Bool {
+        return true
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func drawFocusRingMask() {
+        NSBezierPath.fillRect(bounds)
+    }
+    
+    override var focusRingMaskBounds: NSRect {
+        return bounds
+    }
+    
+    
 }
